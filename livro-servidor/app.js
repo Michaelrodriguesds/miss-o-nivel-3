d76a6e61ef5435da6e35a99aa84bb9231110f33b
Michael Rodrigues dos Santos <109188658+Michaelrodriguesds@users.noreply.github.com>
2023-09-15 21:53:08 -0300

    Update app.js

--- livro-servidor/app.js
+++ livro-servidor/app.js
@@ -26,12 +26,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/livros', livroRouter);
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
